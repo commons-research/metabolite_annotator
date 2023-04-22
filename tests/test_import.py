95e1010559d3dfa96928b64830ce565bb5b64b56
PMA <pierre-marie.allard@unifr.ch>
2023-04-22 20:18:55 +0200

    configs

---
+++ tests/test_import.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Test the module can be imported."""
+
+import unittest
+
+
+class TestImport(unittest.TestCase):
+    """A test case for import tests."""
+
+    def test_import(self):
+        """Test that metabolite_annotator can be imported."""
+        import metabolite_annotator
